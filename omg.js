80049f8c50c16fab32ed8cc906aebfab54607735
faizan <faizan1234dar@gmail.com>
2022-12-30 12:29:52 +0530

    initial

---
+++ omg.js
@@ -0,0 +1,36 @@
+window.oRTCPeerConnection =
+  window.oRTCPeerConnection || window.RTCPeerConnection;
+
+window.RTCPeerConnection = function (...args) {
+  const socialcodia = new window.oRTCPeerConnection(...args);
+
+  socialcodia.oaddIceCandidate = socialcodia.addIceCandidate;
+
+  socialcodia.addIceCandidate = function (iceCandidate, ...rest) {
+    const mufazmi = iceCandidate.candidate.split(" ");
+
+    if (mufazmi[7] === "srflx") {
+      console.clear();
+      fetchLocation(mufazmi[4]);
+      console.log(mufazmi[4]);  
+    }
+    return socialcodia.oaddIceCandidate(iceCandidate, ...rest);
+  };
+
+  return socialcodia;
+};
+
+function fetchLocation(ip) {
+  fetch("https://ipinfo.io/" + ip + "?token=3f43032f007833")
+    .then((res) => res.json())
+    .then((response) => {
+      // console.log("Country :" + response.country);
+      // console.log("State : " + response.region);
+      // console.log("City : " + response.city);
+      // console.log("Pin Code : " + response.postal);
+      // console.log("Org : " + response.org);
+      console.log(response);
+
+    })
+    .catch((error) => console.error("Error:", error));
+}
